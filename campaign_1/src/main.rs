0e9f3b09bcb85d912a2e4046b6f62c9d9b3be2a6
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2022-03-12 03:02:09 +0530

    proper syntax

--- campaign_1/src/main.rs
+++ campaign_1/src/main.rs
@@ -8,28 +8,14 @@ fn main() {
     println!("The youngest member in the company is {} years old.", youngest);
     println!("The oldest member in the company is {} years old.", oldest);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // //declaring the tuple
-    // let x: (i32, char, &str) = (25, 'F', "Harley");
-    // //accessing the variables
-    // let age = x.0;
-    // let gender = x.1;
-    // let name = x.2;
-    // //printing the numbers
-    // println!("{0} is {1} years old this year. Gender = {2}", name, age, gender);
+}
+fn tuple(){
+    //declaring the tuple
+    let x: (i32, char, &str) = (25, 'F', "Harley");
+    //accessing the variables
+    let age = x.0;
+    let gender = x.1;
+    let name = x.2;
+    //printing the numbers
+    println!("{0} is {1} years old this year. Gender = {2}", name, age, gender);
 }
